2458592cabe57ae7959b11e12a2e56efc6d26e52
MacarenaB <macarenabarba2@gmail.com>
2026-03-07 18:12:44 +0100

    primer ejercicio

--- 001-LISTAS.py
+++ 001-LISTAS.py
@@ -1,1 +1,14 @@
 #Vamos a realizar ejercicios relacionados con las listas.
+
+"""EJERCICIO 1: El Supermercado
+Crea una lista llamada compra que contenga: "pan", "leche", "huevos" y "manzanas"."""
+
+compra=["pan","leche","huevos","manzanas"]
+#A.Imprime el segundo elemento de la lista.
+print(compra[1])
+#B.Añade "chocolate" al final de la lista.
+compra.append("chocolate")
+print(compra)
+#C.Sustituye "leche" por "avena".
+compra[1]="avena"
+print(compra)
