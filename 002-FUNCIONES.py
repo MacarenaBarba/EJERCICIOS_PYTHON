d9b7b259ed0159f52436132b5111f74bc37f38c7
MacarenaB <macarenabarba2@gmail.com>
2026-03-11 17:06:28 +0100

    cambios python

--- 002-FUNCIONES.py
+++ 002-FUNCIONES.py
@@ -24,21 +24,20 @@ print(resultado2)
 """4. Calculadora de Promedio
 Crea una función llamada promedio que reciba una lista de números y devuelva la media aritmética."""
 
-listanumeros=[] 
-def promedio(numeros):
-    mediaAritmetica=sum(numeros)/len(listanumeros)
-    return mediaAritmetica
-   
-
+lista=[]
+#liSTA PROMEDIO
 while True:
-    numeros=(input("Introduce un número:")) #Aqui sera formato string
-    
-    if numeros=="salir":
+    numero=(input("numero:"))
+    if numero.lower()=="salir":
         break
-    numerosInt=int(numeros)    
-    listanumeros.append(numerosInt)
-print(listanumeros)
-resultadoMedia=promedio(listanumeros)
+    numeroInt=int(numero) 
+    lista.append(numeroInt)   
+
+def promedio(datos):
+    media=sum(datos)/len(datos)
+    return media
+
+print(promedio(lista))
 
 #-----------------------------
 #Define una función recursiva que calcule el factorial de n.
@@ -65,3 +64,86 @@ def contarVocales(cadena):
             TotalVocales=len(listaVocales)   #no sum porque son letras
     return TotalVocales
 print(f"El número totales de vocales es: {contarVocales(cadena)}")
+
+#-----------------------------------------------
+# Cuenta cuántas letras mayúsculas y minúsculas hay en una frase
+# Devuelve ambos resultados
+
+def size (cadena):
+    lower=[]
+    higger=[]
+    
+    
+    for i in cadena:
+        if  i.islower():
+            lower.append(i)
+        else:
+            higger.append(i)
+    return len(lower),len(higger)
+
+cadena=input("el texto a introducir: ")
+may,min= size(cadena)
+print(f"minúscula:{min}")
+print(f"mayúscula{may}")
+
+
+
+
+#-----------------------------------------------------------
+# Crea una función llamada calculadora que reciba:
+# - dos números
+# - una operación (+, -, *, /)
+# Y devuelva el resultado
+
+# Esperado:
+# calculadora(10, 2, "+")  → 12
+# calculadora(10, 2, "-")  → 8
+# calculadora(10, 2, "*")  → 20
+# calculadora(10, 2, "/")  → 5
+# calculadora(10, 0, "/")  → "Error: no se puede dividir entre cero"
+
+
+def calculadora(numeroA,numeroB,operation):
+    
+    if operation=="+":
+        resultado=numeroA + numeroB
+    elif operation=="-":
+        resultado=numeroA - numeroB
+    elif operation=="*":
+        resultado=numeroA * numeroB
+    elif operation=="/":
+        resultado=numeroA / numeroB        
+    else:
+        return "Error: operación no válida"
+    return resultado
+
+numeroA=int(input("el primer número es:"))
+numeroB=int(input("El segundo número es:"))
+operation=input("operación (+, -, *, /)")
+
+print(f"el resultado de la calculadora es{calculadora(numeroA,numeroB,operation)}")
+
+#---------------------------------------------------------------------------------------------------
+# # Crea una función que reciba una contraseña y compruebe si es segura.
+# Una contraseña es segura si tiene:
+# - Al menos 8 caracteres
+# - Al menos una mayúscula
+# - Al menos un número
+
+def validador(password):
+    mayuscula=0
+    numero=0
+    lista=[]
+    for i in password:
+        if i.isdigit():
+            numero+=1
+        if i.isupper() :
+            mayuscula+=1
+    
+    if len(password)>=8 and numero>=1 and mayuscula>=1:
+        return"la lista es válida"
+    else:
+        return "La contraseña no es válida"
+password=input("Introduce una password: ")
+print(validador(password))
+        
--- repaso.py
+++ repaso.py
@@ -1,11 +1,23 @@
-listaVocales=[]
-vocales=["a","e","i","o","u"]
-def contarVocales(cadena):
+# # Crea una función que reciba una contraseña y compruebe si es segura.
+# Una contraseña es segura si tiene:
+# - Al menos 8 caracteres
+# - Al menos una mayúscula
+# - Al menos un número
+
+def validador(password):
+    mayuscula=0
+    numero=0
+    lista=[]
+    for i in password:
+        if i.isdigit():
+            numero+=1
+        if i.isupper() :
+            mayuscula+=1
     
-    for i in cadena:
-        if i in vocales:
-            listaVocales.append(i)
-            suma=len(listaVocales)
-    return suma
-cadena: str=input("El texto a introducir:")
-print(f"El numero de vocales es: {contarVocales(cadena)}")
+    if len(password)>=8 and numero>=1 and mayuscula>=1:
+        return"la lista es válida"
+    else:
+        return "La contraseña no es válida"
+password=input("Introduce una password: ")
+print(validador(password))
+        
